75ad0526b752abbd67fe0e777da92daf46b51d41
stari <starikovkost2004@mail.ru>
2022-10-20 21:09:19 +0700

    k

--- app/src/main/java/com/example/pr17/MainActivity.kt
+++ app/src/main/java/com/example/pr17/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : Activity(), View.OnClickListener {
     var btnWeb: Button? = null
@@ -43,6 +46,52 @@ class MainActivity : Activity(), View.OnClickListener {
                 intent.data = Uri.parse("tel:8805553535")
                 startActivity(intent)
             }
+        }
+    }
+    class MainActivity : Activity(), View.OnClickListener {
+        var tvOut: TextView? = null
+        var btnOk: Button? = null
+        var btnCancel: Button? = null
+
+        /** Called when the activity is first created.  */
+        public override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            // найдем View-элементы
+            Log.d(TAG, "найдем View-элементы")
+            tvOut = findViewById<View>(R.id.tvOut) as TextView
+            btnOk = findViewById<View>(R.id.btnOk) as Button
+            btnCancel = findViewById<View>(R.id.btnCancel) as Button
+
+            // присваиваем обработчик кнопкам
+            Log.d(TAG, "присваиваем обработчик кнопкам")
+            btnOk!!.setOnClickListener(this)
+            btnCancel!!.setOnClickListener(this)
+        }
+
+        override fun onClick(v: View) {
+            // по id определяем кнопку, вызвавшую этот обработчик
+            Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик")
+            when (v.id) {
+                R.id.btnOk -> {
+                    // кнопка ОК
+                    Log.d(TAG, "кнопка ОК")
+                    tvOut!!.text = "Нажата кнопка ОК"
+                    Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show()
+                }
+                R.id.btnCancel -> {
+                    // кнопка Cancel
+                    Log.d(TAG, "кнопка Cancel")
+                    tvOut!!.text = "Нажата кнопка Cancel"
+                    Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+
+        companion object {
+            private const val TAG = "myLogs"
         }
     }
 }
